93f1c3a00cfb397c882cdab8506f308c259888ea
rickyarmnd <rickyarmnd@gmail.com>
2023-11-25 00:40:11 +0700

    remove class index

--- index.py
+++ index.py
@@ -2,39 +2,38 @@ import os
 import shutil
 from ConvertVideo import convertVideo
 from RegexSearch import regexSearch
-class index:
+
+rootFolder = '../video-bahasa-isyarat' # tempat penyimpanan video
+saveFolder = 'result' #lokasi penyimpanan video
+
+def open_dir_folder(rootFolder,saveFolder):
     
-    rootFolder = '../video-bahasa-isyarat' # tempat penyimpanan video
-    saveFolder = 'result' #lokasi penyimpanan video
-    
-    def open_dir_folder(rootFolder,saveFolder):
+    for foldername , subfolder , filenames in os.walk(rootFolder):
         
-        for foldername , subfolder , filenames in os.walk(rootFolder):
+        for filename in filenames: 
             
-            for filename in filenames: 
+            locationFile = os.path.join(foldername,filename)
+            
+            folderName = regexSearch.create_folder_name(filename)
+            folderLocation = os.path.join(saveFolder , folderName)
+            
+            checkFileExits = os.path.join(folderLocation,filename.split('.')[0] + '.mp4')
+            
+            if os.path.exists(checkFileExits):
+                print( checkFileExits + ' already exists')
                 
-                locationFile = os.path.join(foldername,filename)
+                continue
+            
+            else:  
                 
-                folderName = regexSearch.create_folder_name(filename)
-                folderLocation = os.path.join(saveFolder , folderName)
+                os.makedirs(folderLocation)
                 
-                checkFileExits = os.path.join(folderLocation,filename.split('.')[0] + '.mp4')
+                if not filename.endswith('.mp4') and not filename.endswith('.MP4'):
+                    print('file' + filename + ' not mp4, converting file to mp4....')
+                    convertVideo.convert_video_to_mp4(filename,locationFile,folderLocation)
                 
-                if os.path.exists(checkFileExits):
-                    print( checkFileExits + ' already exists')
-                    
-                    continue
-                
-                else:  
-                    
-                    os.makedirs(folderLocation)
-                    
-                    if not filename.endswith('.mp4') and not filename.endswith('.MP4'):
-                        print('file' + filename + ' not mp4, converting file to mp4....')
-                        convertVideo.convert_video_to_mp4(filename,locationFile,folderLocation)
-                    
-                    else:
-                        print('doing duplicate file ' + filename + ' to folder result....')
-                        shutil.copy(locationFile, os.path.join(folderLocation, os.path.basename(filename)))
-    
-    open_dir_folder(rootFolder, saveFolder)
+                else:
+                    print('doing duplicate file ' + filename + ' to folder result....')
+                    shutil.copy(locationFile, os.path.join(folderLocation, os.path.basename(filename)))
+
+open_dir_folder(rootFolder, saveFolder)
